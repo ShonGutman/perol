fab7a5839fed5043cde6efbff7c6dfae3e30560f
daniel <danie.zinger@gmail.com>
2024-08-23 23:40:31 +0300

    create seperate thread for listening and writing commands

--- perolServer/server.cpp
+++ perolServer/server.cpp
@@ -1,5 +1,29 @@
 #include "server.h"
 
+// server commands
+#define EXIT "exit" // ends server run
+
 void server::run()
 {
+	// creating listening thread
+	thread t_listen(&server::startListening, this);
+	t_listen.detach();
+
+
+	std::string input;
+	while (input != EXIT)
+	{
+		// get commands from console
+		std::getline(std::cin, input);
+
+		if (input == EXIT)
+			cout << "Goodbye!" << endl;
+		else
+			cout << "Please enter a suported command." << endl;
+	}
+}
+
+void server::startListening()
+{
+	
 }
--- perolServer/server.h
+++ perolServer/server.h
@@ -26,4 +26,5 @@ public:
 
 private:
 
+	void startListening();
 };
