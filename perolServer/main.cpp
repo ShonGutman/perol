e593523d1d1e76991ce421aa1bd5e2b1b63fe8ba
daniel <danie.zinger@gmail.com>
2024-08-23 23:25:58 +0300

    created empty server class

---
+++ perolServer/main.cpp
@@ -0,0 +1,11 @@
+#include "server.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << "Initializing server" << std::endl;
+    server p2pServer();
+
+    std::cout << std::endl << "Successfully run" << std::endl;
+    return 0;
+}
--- perolServer/server.h
+++ perolServer/server.h
@@ -1,2 +1,12 @@
 #pragma once
 #include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/array.hpp>
+#include <thread>
+#include <string>
+#include <iostream>
+
+class server
+{
+
+};
